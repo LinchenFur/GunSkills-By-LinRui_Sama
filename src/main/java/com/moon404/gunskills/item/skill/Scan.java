3dba4c6a0244de520d078efb6905eabe8a0c4422
Moon-404 <zhouzihao2008@126.com>
2024-06-24 19:26:49 +0800

    Update Scan.java

--- src/main/java/com/moon404/gunskills/item/skill/Scan.java
+++ src/main/java/com/moon404/gunskills/item/skill/Scan.java
@@ -1,9 +1,12 @@
 package com.moon404.gunskills.item.skill;
 
 import com.moon404.gunskills.init.GunSkillsEffects;
+import com.moon404.gunskills.message.GlowMessage;
 import com.moon404.gunskills.struct.ClassType;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 
 public class Scan extends SkillItem
@@ -45,24 +48,8 @@ public class Scan extends SkillItem
             String message = "最近的敌人在 " + (int)mindis + " 格外";
             Component component = Component.literal(message);
             player.displayClientMessage(component, true);
-            /*
-            LaserInfo laser = new LaserInfo();
-            laser.from = new Vec3(nearest.getX(), 0, nearest.getZ());
-            laser.length = 100;
-            laser.xRot = -90;
-            laser.yRot = 0;
-            laser.size = 60; // 5秒
-            laser.aiming = 0;
-            laser.isShooter = 0;
-            RenderLaserMessage.INSTANCE.send(PacketDistributor.PLAYER.with(() ->
-            {
-                if (player instanceof ServerPlayer serverPlayer)
-                {
-                    return serverPlayer;
-                }
-                return null;
-            }), laser);
-            */
+            nearest.addEffect(new MobEffectInstance(MobEffects.GLOWING, 60));
+            GlowMessage.sendToTeam(player.getTeam(), nearest, 60);
         }
         return true;
     }
