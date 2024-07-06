5809ccde2e1ea67cb5ac46afc8b1b94c9921f98a
Moon-404 <zhouzihao2008@126.com>
2024-07-06 17:05:05 +0800

    禁用视角切换

--- src/main/java/com/moon404/gunskills/init/GunSkillsKeyMappings.java
+++ src/main/java/com/moon404/gunskills/init/GunSkillsKeyMappings.java
@@ -1,9 +1,13 @@
 package com.moon404.gunskills.init;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import com.mojang.blaze3d.platform.InputConstants;
 import com.moon404.gunskills.GunSkills;
 
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,5 +23,9 @@ public class GunSkillsKeyMappings
     public static void registerBindings(RegisterKeyMappingsEvent event)
     {
         event.register(PING_KEY);
+        Minecraft mc = Minecraft.getInstance();
+        Options options = mc.options;
+        mc.options.keyMappings = ArrayUtils.addAll((KeyMapping[])(new KeyMapping[]{options.keyAttack, options.keyUse, options.keyUp, options.keyLeft, options.keyDown, options.keyRight, options.keyJump, options.keyShift, options.keySprint, options.keyDrop, options.keyInventory, options.keyChat, options.keyPlayerList, options.keyPickItem, options.keyCommand, options.keySocialInteractions, options.keyScreenshot, /*options.keyTogglePerspective,*/ options.keySmoothCamera, options.keyFullscreen, options.keySpectatorOutlines, options.keySwapOffhand, options.keySaveHotbarActivator, options.keyLoadHotbarActivator, options.keyAdvancements}), (KeyMapping[])options.keyHotbarSlots);
+        options.keyTogglePerspective.setKey(InputConstants.UNKNOWN);
     }
 }
