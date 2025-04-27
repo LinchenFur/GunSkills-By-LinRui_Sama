e6143a880c25376b3ac9116aa71020f4050d72bf
Moon-404 <33384538+Moon-404@users.noreply.github.com>
2025-04-27 15:30:34 +0800

    0.4.9 攀爬更新
    1. 攀爬/滑墙持续时间从1秒延长至1.5秒
    2. 滑墙现在需要玩家视线与墙壁有一定夹角，避免了想要攀爬时误触发

--- src/main/java/com/moon404/gunskills/animation/Climb.java
+++ src/main/java/com/moon404/gunskills/animation/Climb.java
@@ -6,6 +6,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent.Phase;
@@ -29,21 +32,24 @@ public class Climb
             Minecraft minecraft = Minecraft.getInstance();
             Options options = minecraft.options;
             Vec3 delta = player.getDeltaMovement();
-            if (player.horizontalCollision && !player.onGround() && delta.y <= 0.1 && tickCount < 20 && options.keyUp.isDown())
+            if (player.horizontalCollision && !player.onGround() && delta.y <= 0.1 && tickCount < 30 && options.keyUp.isDown())
             {
                 tickCount++;
-                boolean flag = options.keyLeft.isDown() || options.keyRight.isDown();
-                if (flag)
+                Vec3 pos = player.position();
+                Vec3 look = player.getLookAngle().multiply(1, 0, 1).normalize();
+                BlockHitResult blockHitResult = player.level().clip(new ClipContext(pos, pos.add(look), ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player));
+
+                if (blockHitResult.getType() != HitResult.Type.BLOCK && (options.keyLeft.isDown() || options.keyRight.isDown()))
                 {
                     float speed = player.getSpeed() * 2;
                     Vec3 movevec = new Vec3(0, 0, 0);
-                    if (delta.x == 0) movevec = player.getForward().multiply(0, 0, 1).normalize().scale(speed);
-                    else if (delta.z == 0) movevec = player.getForward().multiply(1, 0, 0).normalize().scale(speed);
+                    if (delta.x == 0) movevec = look.multiply(0, 0, 1).normalize().scale(speed);
+                    else if (delta.z == 0) movevec = look.multiply(1, 0, 0).normalize().scale(speed);
                     player.setDeltaMovement(movevec);
 
                     if (!jumping && options.keyJump.isDown())
                     {
-                        Vec3 jumpvec = player.getForward().multiply(1, 0, 1).normalize().scale(speed * 2);
+                        Vec3 jumpvec = look.scale(speed * 2);
                         double dy = 0.3;
                         if (player.hasEffect(MobEffects.JUMP)) dy *= 1.2 + 0.2 * player.getEffect(MobEffects.JUMP).getAmplifier();
                         jumpvec = jumpvec.add(0, dy, 0);
